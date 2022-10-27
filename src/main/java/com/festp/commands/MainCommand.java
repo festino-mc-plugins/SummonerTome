0f61bb89d1dfd8baaa636897a230570ec2ec7f56
Festino <offestashka@mail.ru>
2022-10-27 23:49:46 +0300

    New transparency/solidity checker

--- src/main/java/com/festp/commands/MainCommand.java
+++ src/main/java/com/festp/commands/MainCommand.java
@@ -1,14 +1,10 @@
 package com.festp.commands;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Map.Entry;
 
 import org.bukkit.ChatColor;
-import org.bukkit.FluidCollisionMode;
-import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.command.Command;
@@ -19,9 +15,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
-import org.bukkit.util.RayTraceResult;
-import org.bukkit.util.Vector;
-
 import com.festp.handlers.TomeItemHandler;
 import com.festp.tome.TomeType;
 import com.festp.utils.UtilsType;
@@ -37,50 +30,6 @@ public class MainCommand  implements CommandExecutor, TabCompleter
 	private final static String NO_PLAYER = ChatColor.RED + "You must be a player to perform this command.";
 	private final static String NO_OP = ChatColor.RED + "You must be an operator to perform this command.";
 	private final static String NO_SPACE = ChatColor.RED + "There are no space in the inventory.";
-
-	private static Map<Material, Boolean> isTransparent = new HashMap<>(); // entity can stay in (no liquids)
-	private static Map<Material, Boolean> isSolid = new HashMap<>(); // entity can stay on (no liquids)
-
-	// errors: gates(opened/closed)
-	private static void checkBlock(Block b)
-	{
-		Material m = b.getType();
-		if (isTransparent.containsKey(m))
-			return;
-		
-		double eps = 0.001;
-		Vector[] localStarts = new Vector[] {
-				new Vector(eps, eps, eps), new Vector(1 - eps, eps, eps),
-				new Vector(eps, 1 - eps, eps), new Vector(1 - eps, 1 - eps, eps),
-				new Vector(0, 0.5, 0.5), new Vector(0.5, 0, 0.5), new Vector(0.5, 0.5, 0) };
-		Vector[] directions = new Vector[] {
-				new Vector(1, 1, 1), new Vector(-1, 1, 1),
-				new Vector(1, -1, 1), new Vector(-1, -1, 1),
-				new Vector(1, 0, 0), new Vector(0, 1, 0), new Vector(0, 0, 1)};
-		double sqrt3 = Math.sqrt(3);
-		double[] dists = new double[] { sqrt3, sqrt3, sqrt3, sqrt3, 1, 1, 1 };
-		for (int i = 0; i < localStarts.length; i++)
-		{
-			Location start = b.getLocation().add(localStarts[i]);
-			double dist = dists[i] - 2 * eps;
-			RayTraceResult result = b.getWorld().rayTraceBlocks(start, directions[i], dist, FluidCollisionMode.NEVER, true);
-			if (result != null) {
-				isSolid.put(m, true);
-				isTransparent.put(m, false);
-				return;
-			}
-			else {
-				result = b.getWorld().rayTraceBlocks(start, directions[i], dist, FluidCollisionMode.NEVER, false);
-				if (result != null || UtilsType.isAir(m)) {
-					isTransparent.put(m, true);
-					isSolid.put(m, false);
-					return;
-				}
-			}
-		}
-		isTransparent.put(m, false);
-		isSolid.put(m, false);
-	}
 	
 	List<String> enNames = new ArrayList<>();
 	
@@ -146,11 +95,11 @@ public class MainCommand  implements CommandExecutor, TabCompleter
 				for (int dy = yMin; dy <= yMax; dy++)
 					for (int dx = -10; dx <= 10; dx++)
 						for (int dz = -10; dz <= 10; dz++)
-							checkBlock(b.getRelative(dx, dy, dz));
+							UtilsType.isTransparent(b.getRelative(dx, dy, dz));
 				
 				sender.sendMessage(ChatColor.GREEN + "" + ChatColor.BOLD + "Transparent | Solid:");
-				for (Entry<Material, Boolean> entry : isTransparent.entrySet())
-					sender.sendMessage(ChatColor.GREEN + "    " + entry.getValue() + " | " + isSolid.get(entry.getKey()) + " : " + entry.getKey());
+				for (Entry<Material, Boolean> entry : UtilsType.getIsTransparent().entrySet())
+					sender.sendMessage(ChatColor.GREEN + "    " + entry.getValue() + " | " + UtilsType.getIsSolid().get(entry.getKey()) + " : " + entry.getKey());
 				return true;
 			}
 
