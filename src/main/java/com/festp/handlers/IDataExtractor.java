11b5a8c8b777291dbf708f97c2a9b67a3cc1ee5d
Festino <offestashka@mail.ru>
2023-07-02 19:49:22 +0300

    Added class description

--- src/main/java/com/festp/handlers/IDataExtractor.java
+++ src/main/java/com/festp/handlers/IDataExtractor.java
@@ -5,6 +5,7 @@ import org.bukkit.inventory.ItemStack;
 
 import com.festp.components.ITomeComponent;
 
+/** Determines what changes can be saved while the entity is summoned */
 public interface IDataExtractor
 {
 	public ITomeComponent extract(ItemStack oldTome, Entity entity);
